d3254065242b42129a2979cf3bd3b36268747c52
dmcclellan3 <wdmcclellan3@gmail.com>
2024-03-29 10:25:52 -0400

    latest

--- main.js
+++ main.js
@@ -8,4 +8,13 @@ setInterval(()=>{
     hrs.innerHTML = (currentTime.getHours()<10?"0":"") + currentTime.getHours();
     min.innerHTML = (currentTime.getMinutes()<10?"0":"") + currentTime.getMinutes();
     sec.innerHTML = (currentTime.getSeconds()<10?"0":"") + currentTime.getSeconds();
-},1000)
+})
+
+setTimeout(function(){
+    let alarmTime = document.getElementById("alarmTime");
+     
+})
+
+setAlarmBtn.addEventListener('click', () =>{
+    
+})
