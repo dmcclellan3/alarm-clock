379df6ef5b08c03571aa2589ad8c1e6e254b9cdc
dmcclellan3 <wdmcclellan3@gmail.com>
2024-03-28 21:37:44 -0400

    latest

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
 
-let currentTime = new Date();
+setInterval(()=>{
+    let currentTime = new Date();
 
-hrs.innerHTML = currentTime.getHours();
-min.innerHTML = currentTime.getMinutes();
-sec.innerHTML = currentTime.getSeconds();
+    hrs.innerHTML = currentTime.getHours()<10?"0":"" + currentTime.getHours();
+    min.innerHTML = currentTime.getMinutes()<10?"0":"" + currentTime.getMinutes();
+    sec.innerHTML = currentTime.getSeconds()<1?"0":"" + currentTime.getSeconds();
+},1000)
